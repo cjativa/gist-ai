acebb94d5506a10e2d259c1c11543afc1430ddaf
Christopher Jativa <Christopher.jativa320@outlook.com>
2024-09-26 12:36:47 -0400

    add json type

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    singleQuote: true,
-    trailingComma: 'es5',
-    plugins: [],
-    overrides: [
-        {
-            files: '*.{js,ts,gjs,gts}',
-            options: {
-                singleQuote: true,
-                templateSingleQuote: false
-            }
-        },
-        {
-            files: '*.hbs',
-            options: {
-                singleQuote: false
-            }
-        }
-    ]
+  singleQuote: true,
+  trailingComma: 'es5',
+  plugins: [],
+  overrides: [
+    {
+      files: '*.{js,ts,gjs,gts,json}',
+      options: {
+        singleQuote: true,
+        templateSingleQuote: false,
+      },
+    },
+    {
+      files: '*.hbs',
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
