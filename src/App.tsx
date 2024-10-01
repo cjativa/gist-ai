aeef78527b042e85dc9d82582ed68478f34f0f31
Christopher Jativa <Christopher.jativa320@outlook.com>
2024-10-01 09:57:36 -0400

    work on identifying intents

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {
+  styled,
   TextField,
   Box,
-  styled,
   Typography,
+  Button,
+  ButtonGroup,
   type BoxProps,
 } from '@mui/material';
 
@@ -13,26 +15,33 @@ import { IntentMessage, IntentTypes } from './types';
 
 const IntentInformationMap: {
   [key in keyof typeof IntentTypes]: {
-    label: string;
+    actionOperationLabel: string;
+    actionButtonLabel: string;
   };
 } = {
   [IntentTypes.summarizer]: {
-    label: 'Summarizing this for you...',
+    actionOperationLabel: 'Summarizing this for you...',
+    actionButtonLabel: 'Summarize it',
   },
   [IntentTypes.explainer]: {
-    label: 'Explaining it better for you...',
+    actionOperationLabel: 'Explaining it better for you...',
+    actionButtonLabel: 'Explain it',
   },
   [IntentTypes.actioner]: {
-    label: 'Coming up with an action plan for you based off this...',
+    actionOperationLabel:
+      'Coming up with an action plan for you based off this...',
+    actionButtonLabel: 'Build an action plan',
   },
   [IntentTypes.rephraser]: {
-    label: 'Rephrasing this in a way you might say it...',
+    actionOperationLabel: 'Rephrasing this in a way you might say it...',
+    actionButtonLabel: 'Rephrase this',
   },
 };
 
 const StyledAppContainer = styled(Box)<BoxProps>(({ theme }) => ({
   width: '40em',
   height: '40em',
+  padding: '1em',
 }));
 
 const StyledHeader = styled(Box)(({ theme }) => ({
@@ -42,9 +51,31 @@ const StyledHeader = styled(Box)(({ theme }) => ({
   alignItems: 'center',
 }));
 
+const StyledIntentContainer = styled(Box)(({ theme }) => ({
+  marginTop: '1em',
+
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  rowGap: '1em',
+}));
+
+enum IntentSources {
+  ContextMenu = 'ContextMenu',
+  ExtensionEntrypoint = 'ExtensionEntrypoint',
+}
+
+const RenderForMessageIntent = () => {};
+
+const RenderForPopupIntent = () => {};
+
 export function Application() {
   const [intentConent, setIntentContent] = React.useState('');
   const [intentId, setIntentId] = React.useState<null | IntentTypes>(null);
+  const [intentSource, setIntentSource] = React.useState<null | IntentSources>(
+    null
+  );
 
   const intentInformation = intentId ? IntentInformationMap[intentId] : null;
 
@@ -52,6 +83,7 @@ export function Application() {
     message: IntentMessage,
     sender: chrome.runtime.MessageSender
   ): boolean {
+    setIntentSource(IntentSources.ContextMenu);
     setIntentId(message.intentTypeId);
     setIntentContent(message.content);
 
@@ -60,12 +92,20 @@ export function Application() {
 
   /** Set-up the runtime message handler once */
   React.useEffect(() => {
-    /** Configure our listener from our content script to the main application */
-    chrome?.runtime?.onMessage.addListener(handleMessageFromBackground);
+    // Configure our listener from our content script to the main application
+    if (chrome?.runtime) {
+      chrome.runtime.onMessage.addListener(handleMessageFromBackground);
+    }
+
+    // Otherwise, the extension application was opened directly from the extension
+    // toolbar entrypoint. Let's allow the end-user to add some input text and select their intent
+    else {
+      setIntentSource(IntentSources.ExtensionEntrypoint);
+    }
   }, []);
 
   return (
-    <StyledAppContainer height={'30em'} width={'30em'}>
+    <StyledAppContainer>
       <div className="App">
         <StyledHeader>
           <img src={logo} className="App-logo" alt="logo" />
@@ -77,10 +117,47 @@ export function Application() {
           </Typography>
         </StyledHeader>
 
+        {/** When the intent source is the extension entrypoint itself, we'll render the input field
+         * and the button group for the end-user to make a selection of intent
+         */}
+        {intentSource === IntentSources.ExtensionEntrypoint ? (
+          <StyledIntentContainer>
+            <Typography variant="body2">
+              Choose one of the actions below
+            </Typography>
+
+            <ButtonGroup variant="contained" aria-label="Gist action buttons">
+              {Object.entries(IntentInformationMap).map(
+                ([intentId, intentItem]) => {
+                  return (
+                    <Button key={intentId}>
+                      {intentItem.actionButtonLabel}
+                    </Button>
+                  );
+                }
+              )}
+            </ButtonGroup>
+
+            <TextField
+              id="outlined-read-only-input"
+              label="Content"
+              value={intentConent}
+              multiline
+              fullWidth
+              slotProps={{
+                input: {
+                  readOnly: true,
+                },
+              }}
+            />
+          </StyledIntentContainer>
+        ) : null}
+
+        {/** Once we have a desired intent and intent content, we can render the below */}
         {intentInformation ? (
-          <div>
+          <StyledIntentContainer>
             <p>Current Intent</p>
-            <p>{intentInformation.label}</p>
+            <p>{intentInformation.actionOperationLabel}</p>
 
             <p>Content being used</p>
             <TextField
@@ -94,7 +171,7 @@ export function Application() {
                 },
               }}
             />
-          </div>
+          </StyledIntentContainer>
         ) : null}
       </div>
     </StyledAppContainer>
