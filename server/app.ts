d53299fe5828f2cacd06f8f1ddda4eb9173e7988
Christopher Jativa <Christopher.jativa320@outlook.com>
2024-10-01 20:00:40 -0400

    using api router

--- server/app.ts
+++ server/app.ts
@@ -1,1 +1,18 @@
-export const application = () => console.log('Hi');
+import express from 'express';
+
+import { ApiRouter } from './api/routes';
+import { Config } from './config';
+
+const LOG_PREFIX = '[Application]';
+
+const application = express();
+
+// Configure our API router
+application.use('/api', ApiRouter);
+
+// Configure the server start
+application.listen(Config.port, () => {
+  console.log(`${LOG_PREFIX} Server listening on port ${Config.port}`);
+});
+
+export { application };
