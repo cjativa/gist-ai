19a920bfd276d4e72745b347e18aea744f5db25b
Christopher Jativa <Christopher.jativa320@outlook.com>
2024-10-03 18:03:34 -0400

    add sourcemaps for debugging

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import { ApiRouter } from './api/routes';
 import { Config } from './config';
@@ -8,6 +9,8 @@ const LOG_PREFIX = '[Application]';
 const application = express();
 
 // Configure our API router
+application.use(express.urlencoded({ extended: true }));
+application.use(bodyParser.json());
 application.use('/api', ApiRouter);
 
 // Configure the server start
