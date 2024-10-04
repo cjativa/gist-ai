3e7ad443e21d4357693651466c6d7aee4fe7d472
Christopher Jativa <Christopher.jativa320@outlook.com>
2024-10-04 16:39:27 -0400

    lowercase button typography

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -2,6 +2,11 @@ import { createTheme } from '@mui/material/styles';
 
 // A custom theme for this app
 export const MuiTheme = createTheme({
+  typography: {
+    button: {
+      textTransform: 'none',
+    },
+  },
   palette: {
     mode: 'light',
     primary: {
